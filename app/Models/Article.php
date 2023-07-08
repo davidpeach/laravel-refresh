2460cfd203b4dc7f3811d65ba63565c13b5b03e8
David Peach <mail@davidpeach.co.uk>
2023-07-08 19:18:45 +0100

    wip

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,12 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Activity extends Model
+class Article extends Model
 {
     use HasFactory;
-
-    public function feedable()
-    {
-        return $this->morphTo();
-    }
 }
